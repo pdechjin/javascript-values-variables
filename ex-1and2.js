8aa93dbe5fc48ee45fbc188d511ee58a28d2353d
pdechjin <panuwat.dechjinda1234@gmail.com>
2025-09-15 20:30:25 +0700

    assigement:Let check score after get answer

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter"
+let userName = "Anderson"
+let address = "54/24 Gb Bankkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurant = "Chef's Table by Chef James"
+const totalBilling = 15000
+const restaurantOpen = 1500
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalBilling = 5000
+restaurantOpen = 2500
